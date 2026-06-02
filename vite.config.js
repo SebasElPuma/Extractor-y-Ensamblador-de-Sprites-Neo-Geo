f59079098913c17d49c2e463d4d1f25e2b391eae
SebasElPuma <pollitoyolo980@gmail.com>
2026-06-01 21:21:15 -0500

    GHpt2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Extractor-y-Ensamblador-de-Sprites-Neo-Geo/', // <-- AÑADE ESTA LÍNEA (Debe ser el nombre exacto de tu repo)
+  base: '/Extractor-y-Ensamblador-de-Sprites-Neo-Geo/',
 })
